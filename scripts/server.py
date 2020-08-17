9fb47ac17a862b45d5a6cfcec7896e6df1f19e1f
Polgár András <azazdeaz@gmail.com>
2020-08-17 07:43:00 +0200

    :construction: move to noetic and update runnables

--- scripts/server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-try:
-    import generated.grpc_server
-except ImportError as e:
-    print("Can't import generated/grcp_server. Did you run the generate script? (ie. '$ rosrun ros_grpc_wrapper generate')")
-    raise e
-
-generated.grpc_server.parse_args_and_run_server()
